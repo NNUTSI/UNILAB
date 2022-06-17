408cdd42791112ddf11fd2b9201044db321f9fe9
NNUTSI <89682164+NNUTSI@users.noreply.github.com>
2022-06-17 04:17:09 +0400

    burger menu, api, XXXX

--- js/script.js
+++ js/script.js
@@ -13,34 +13,3 @@ function toggleBack() {
     mobileOverlay.classList.remove('active');
     
  }
- const getUsersButton = document.querySelector(".button");
- const cardsContainer = document.querySelector(".card_container");
- const userList = document.getElementById("users");
- const singleUser = async (event) => {
-   const user = event.target.value;
-   console.log(user);
-   const response = await fetch(
-     `https://jsonplaceholder.typicode.com/users?name=${user}`
-   );
-   const [data] = await response.json();
-   console.log(data);
-   console.log(data.name);
-   const userCard = `
-     
-   <article class="user_card">
-     <div class="user"> 
-       <h3 class="user_name">${data.name}</h3>
-       <div class="user_information">
-       <p class="user_username">username: ${data.username}</p>
-       <p class="user_address">city: ${data.address.city}</p>
-       <p class="user_email">email: ${data.email}</p>
-       <p class="country_phone">phone: ${data.phone}</p>
-       </div>
-     </div>
-   </article>
-   `;
-   cardsContainer.insertAdjacentHTML("beforeend", userCard);
- };
- 
- userList.addEventListener("change", singleUser);
-
